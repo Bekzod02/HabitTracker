eac2fbbe1cd5c41802ef9b2b897dff9bd06b37af
Bekzod Khaitboev <bekhzod.khaitboev@gmail.com>
2025-03-25 20:51:06 +0500

    Initial Commit

---
+++ HabitTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HabitTracker
+//
+//  Created by Bekzod Khaitboev on 25/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
